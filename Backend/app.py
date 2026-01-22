29938e612853de42982496e94f0d05a2584962ae
Dwight Thompson <110430088+mr9t3n@users.noreply.github.com>
2026-01-21 22:26:40 -0500

    Routing  Architecture
    Completed checklist items for Routing Architecture card in Trello:< SUBMITTED FOR REVIEW >
    Creation of dev-backend branch
    Implemented Dynamic Routing, JSON endpoints, and Global Error Handling.
    File Updated: backend/app.py
    **This is to form the architectural skeleton that will be build upon with the other cards

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,56 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
+# Home routing for general verification
 @app.route('/')
 def home():
     return "Team 2 Flask Server is Running!"
+
+# API Dynamic API Endpoint             ---------------------->use: http://127.0.0.1:5000/api/test
+# Returns JSON, how we will be communicating with the frontend
+# This route will accept both GET (fetching)  and POST (sending/creating)
+@app.route('/api/test' , methods=['GET', 'POST'])
+def test_endpoint():
+    if request.method == 'POST':
+        # This triggers when Frontend SENDS data
+        data = request.get_json() if request.is_json else {"info": "No JSON received"}
+        return jsonify({
+                "status": "success", 
+                "message": "POST request received!", 
+                "received_data": data
+        }), 201 # std code for 'Created'
+    # This triggers when you visit the url in the browser
+    return jsonify({
+        "status": "success", 
+        "message": "GET request working! Use POST to send data. ", 
+        "phase": 6, 
+        "inProgress card": 1
+     })
+
+# URL Parameter Research
+# This allows the URL to act as data   using any name after api/game/(name) for example:  http://127.0.0.1:5000/api/game/GTA6
+@app.route('/api/game/<name>')
+def get_game_data(name):
+    #'name' is the </placeholder/> when dev is there 'name' will be used to look up data in a particular database
+    return jsonify({
+        "game_name": name, 
+        "status": "active",
+        "message": f"Fetching logic for {name}..."
+        })
+
+# Error Handling
+# Insurance: frontend always receives JSON even on a 404 erro message
+# with anything after http://127.0.0.1:5000 for example: http://127.0.0.1:5000/this_is-notReal
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "status": "error", 
+        "message": "Endpoint not found. Check your URL structure.", 
+        "error_details": str(error)
+        }), 404
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
