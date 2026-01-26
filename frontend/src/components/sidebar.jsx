11f2afe9974c6473afe3eb3ec8cd78026177ac32
NSSung <nsung214@gmail.com>
2026-01-26 12:38:16 -0800

    Defined all design system colors and brand font (Jarkarta sans). Uploaded both brand and sidebar svgs. updated sidebar appearance filling button and color scheme

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,4 +1,6 @@
 import { Button } from "@heroui/react";
+import collapse from '../assets/sidebar-collapse.svg';
+import brand from '../assets/brand.svg';
 
 export default function Sidebar(){
     const pages = [
@@ -18,18 +20,21 @@ export default function Sidebar(){
 
     return (
     <div className='col-span-3'>
-      <div className='h-screen bg-neutral-500'>
-        <div className='flex py-2 px-4 align-center'>
-          <h2 className='content-center text-4xl' color="primary">MyHQ</h2>
-        </div>
-        <nav>
+        <nav className='h-screen bg-neutral-200'>
+          <div className="flex justify-between p-8 content-center">
+            <div>
+              <img width='32' src={brand} alt='brand' />
+            </div>
+            <div>
+              <img width='24' className='py-1 ' src={collapse} alt='collapse' /> {/* TODO: Add Collapsability interaction to the button . Add in hover capability*/}
+            </div>
+          </div>
           {pages.map((page) => (
-              <div key={page.id} className='text-lg p-2'>
-                <Button className='w-full py-4' color="primary" radius="md">{page.name}</Button>
+              <div key={page.id} className='text-xl py-1 px-4 hover:bg-neutral-400 rounded-md'>
+                <Button className='w-full h-16 text-neutral-800' radius="md">{page.name}</Button>
               </div>
           ))}
         </nav>
       </div>
-    </div>
     )
 }
