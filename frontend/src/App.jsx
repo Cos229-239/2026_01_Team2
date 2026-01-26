eff5274ba39e7f5f75f93e943c2eb017d9d02417
NSSung <nsung214@gmail.com>
2026-01-26 11:12:15 -0800

    Removed collapse sidebar button. Replaced myHQ logo on index.html. Uploaded brand.svg. Migrated nested div into the sidebar.jsx. Updated primary color to the brand color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,9 +26,7 @@ function App() {
     <>
       <main className='h-screen w-screen flex-row align-center bg-neutral-100'>
         <div className='grid grid-flow-col grid-cols-12 gap-4'> {/*top layer*/}
-          <div className='col-span-2'> {/* Will be moved into the child component */}
             <Sidebar />
-          </div>
           <div className='flex col-span-10 row-span-1'>
             <div className='w-full'> {/* keep this within the parent for all contnt */}
               <h1 className='text-2xl'>This React Page is Working</h1>
