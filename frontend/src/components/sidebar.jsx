da964d5944c395be6ceeab569503abab03b8cabd
NSSung <nsung214@gmail.com>
2026-01-26 13:03:49 -0800

    removed some unnecessary divs

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -22,16 +22,12 @@ export default function Sidebar(){
     <div className='col-span-3'>
         <nav className='h-screen bg-neutral-200'>
           <div className="flex justify-between p-8 content-center">
-            <div>
               <img width='32' src={brand} alt='brand' />
-            </div>
-            <div>
               <img width='24' className='py-1 ' src={collapse} alt='collapse' /> {/* TODO: Add Collapsability interaction to the button . Add in hover capability*/}
-            </div>
           </div>
           {pages.map((page) => (
               <div key={page.id} className='text-xl py-1 px-4 hover:bg-neutral-400 rounded-md'>
-                <Button className='w-full h-16 text-neutral-800' radius="md">{page.name}</Button>
+                <Button className='flex justify-start h-16 text-neutral-800' radius="md">{page.name}</Button>
               </div>
           ))}
         </nav>
