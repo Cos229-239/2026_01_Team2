dd8c4f81fe6b629e42c408627de6a330d7d522ee
NSSung <nsung214@gmail.com>
2026-01-27 20:49:46 -0800

    Implemented designer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,28 @@
-import { useEffect } from 'react'
-import axios from "axios";
 import Sidebar from './components/sidebar';
-import Designer from "./components/designer";
-import './App.css'
+import Designer from './components/designer';
 
 function App() {
-  //example get response from the backend of flask
-  const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:5000/map");
-    console.log(response.data);
-  }
-  //this runs the function based on an action (in this case, the function being run is only run once at the beginning. '[]' would be the action or function being called)
-  useEffect (() => {
-    fetchAPI()
-  },[])
+
+
+
+  // console.log(gridData);
   // nothing is returned at the moment but will be populated after gathering the information
   return (
     <>
-      <main className='h-screen w-screen flex-col bg-neutral-100'>
+      <main className='h-screen w-screen flex-row align-center bg-neutral-100'>
         <div className='flex'> {/*top layer*/}
-          <Sidebar />
-          <div className='justify-stretch pl-4 pr-4 w-screen h-screen'>
-            <div className='text-center'>
-                <div className='text-2xl'>This React Page is Working</div>
-            </div>
-            
-            <Designer />
+          <div className='col-span-2'> {/* Will be moved into the child component */}
+            <Sidebar />
           </div>
+          <div className='h-full w-full p-4'>
+            <div> {/* keep this within the parent for all contnt */}
+              <h1 className='text-2xl'>This React Page is Working</h1>
+            </div>
+            <div className=''>
+              <Designer />
+            </div>
+          </div>
+
         </div>
       </main>
     </>
--- frontend/src/components/designer.jsx
+++ frontend/src/components/designer.jsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react'
+import axios from "axios";
 
 
 function Designer(){
+    const [ gridData, setGridData ] = useState(null);
+    const [ error, setError ] = useState(null);
 
-
+    //example get response from the backend of flask
+    const fetchAPI = async () => {
+        try {
+            const res = await axios.get("http://localhost:5000/api/game/init");
+            setGridData(res);
+        } catch (err) {
+            setError(err?.message ?? "request failed");
+            console.error(err);
+        }
+    // console.log(response.data);
+    };
+    //this runs the function based on an action (in this case, the function being run is only run once at the beginning. '[]' would be the action or function being called)
+    useEffect (() => {
+    fetchAPI();
+    },[])
+    // console.log(response.data)
     return (
-        <div className="outline-1 outline-neutral-300 rounded-md">
-            {}
+        <div className="outline-1 overflow-hidden outline-neutral-300 rounded-md">
+            {error && <div className = "text-warning-600">Error: {error}</div>}
+            {!gridData ? (
+                <div>Loading...</div>): (
+                    <div>{gridData.data.grid_size}</div>
+                )
+            }
         </div>
     )
 }
